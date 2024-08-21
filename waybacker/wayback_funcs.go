f241a5760092a88ac14a2c8bba145f7556f7b854
Meadow <meadowingc@gmail.com>
2024-08-21 14:25:18 -0600

    specifically don't archive if they have an archive that's newer than 20 days

--- waybacker/wayback_funcs.go
+++ waybacker/wayback_funcs.go
@@ -13,6 +13,7 @@ func SendToWaybackMachine(targetURL, accessKey, accessSecret string) (*http.Resp
 	data.Set("capture_all", "1")
 	data.Set("capture_outlinks", "1")
 	data.Set("delay_wb_availability", "1")
+	data.Set("if_not_archived_within", "20d")
 
 	req, err := http.NewRequest("POST", waybackURL, strings.NewReader(data.Encode()))
 	if err != nil {
